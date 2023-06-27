545df9e2dcaf23ef9c23d6ec47de60ee5a83d92a
Agustin101 <agus_101@live.com.ar>
2023-06-27 18:14:05 -0300

    Modelo finalizado

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,6 @@
 import { SuperHeroe } from "./superHeroe.js";
 import { getHeroes, postHero, updateHero, deleteHero } from "./peticiones.js";
+import { crearFiltrosColumnas } from "./filtros.js";
 
 import {
     crearTabla,
@@ -51,6 +52,24 @@ const limpiarCampos = () => {
 };
 
 const crearAnuncio = async ({ nombre, alias, editorial, fuerza, arma }) => {
+    let hayError = false;
+
+    if (nombre.value.trim() == "") {
+        document.getElementById("validacionNombre").classList.remove("oculto");
+        hayError = true;
+    } else {
+        document.getElementById("validacionNombre").classList.add("oculto");
+    }
+
+    if (alias.value.trim() == "") {
+        document.getElementById("validacionAlias").classList.remove("oculto");
+        hayError = true;
+    } else {
+        document.getElementById("validacionAlias").classList.add("oculto");
+    }
+
+    if (hayError) return Promise.reject("Campos sin completar");
+
     let anuncio = new SuperHeroe(
         0,
         nombre.value,
@@ -61,7 +80,12 @@ const crearAnuncio = async ({ nombre, alias, editorial, fuerza, arma }) => {
     );
 
     try {
-        $form.classList.add("oculto");
+        $form.classList.add("spinner-oculto");
+        document.getElementById("filtrosContainer").classList.add("oculto");
+        document
+            .getElementById("table-container")
+            .classList.add("spinner-oculto");
+        document.querySelector(".spinner").classList.remove("spinner-oculto");
         await postHero("http://localhost:3000/heroes", anuncio);
     } catch (err) {
         console.log(err);
@@ -72,12 +96,28 @@ const actualizarAnuncio = async (
     anuncioId,
     { nombre, alias, editorial, fuerza, arma }
 ) => {
-    const updatedHero = new SuperHeroe(anuncioId, nombre.value, fuerza.value,alias.value,editorial.value,arma.value);
-    console.log(updatedHero);
-    try{
-        await updateHero("http://localhost:3000/heroes",updatedHero,anuncioId);
-    }
-    catch (err){
+    const updatedHero = new SuperHeroe(
+        anuncioId,
+        nombre.value,
+        fuerza.value,
+        alias.value,
+        editorial.value,
+        arma.value
+    );
+
+    try {
+        $form.classList.add("spinner-oculto");
+        document.getElementById("filtrosContainer").classList.add("oculto");
+        document
+            .getElementById("table-container")
+            .classList.add("spinner-oculto");
+        document.querySelector(".spinner").classList.remove("spinner-oculto");
+        await updateHero(
+            "http://localhost:3000/heroes",
+            updatedHero,
+            anuncioId
+        );
+    } catch (err) {
         console.log(err);
     }
 };
@@ -92,6 +132,11 @@ const agregarAnuncio = (e) => {
         actualizarAnuncio(parseInt(anuncioId.value), $form);
     }
     ocultarBotonesEdicion();
+};
+
+const mostrarFiltros = () => {
+    const $filtros = document.getElementById("filtrosContainer");
+    $filtros.classList.remove("oculto");
 };
 
 const iniciarAplicacion = async () => {
@@ -118,6 +163,14 @@ const iniciarAplicacion = async () => {
             crearTabla(anuncios);
             cargarAnuncios(anuncios);
             calcularPromedio(anuncios);
+            crearFiltrosColumnas(anuncios[0]);
+            mostrarFiltros();
+            const checksFiltros = document.querySelectorAll(
+                "div input[type=checkbox]"
+            );
+            checksFiltros.forEach((check) => {
+                check.addEventListener("change", onChangeCheck);
+            });
         }
     } catch (err) {
         console.log(err);
@@ -125,14 +178,9 @@ const iniciarAplicacion = async () => {
         document.querySelector(".spinner").classList.add("spinner-oculto");
         ocultarBotonesEdicion();
     }
-
-    checksFiltros.forEach(check => {
-        check.addEventListener("change", onChangeCheck); 
-    });
 };
 
 const $form = document.forms[0];
-const checksFiltros = document.querySelectorAll("div input[type=checkbox]");
 $form.addEventListener("submit", agregarAnuncio);
 document.addEventListener("DOMContentLoaded", iniciarAplicacion);
 
@@ -145,10 +193,15 @@ document.getElementById("btnEliminar").addEventListener("click", async () => {
     let { anuncioId } = $form;
     anuncioId = parseInt(anuncioId.value);
 
-    try{
-        await deleteHero("http://localhost:3000/heroes",anuncioId);
-    }
-    catch (err){
+    try {
+        $form.classList.add("spinner-oculto");
+        document.getElementById("filtrosContainer").classList.add("oculto");
+        document
+            .getElementById("table-container")
+            .classList.add("spinner-oculto");
+        document.querySelector(".spinner").classList.remove("spinner-oculto");
+        await deleteHero("http://localhost:3000/heroes", anuncioId);
+    } catch (err) {
         console.log(err);
     }
 });
@@ -174,54 +227,63 @@ document.addEventListener("click", (e) => {
     }
 });
 
-document.getElementById("selectFiltros").addEventListener("change", onChangeSelect);
+document
+    .getElementById("selectFiltros")
+    .addEventListener("change", onChangeSelect);
 
-function onChangeCheck(e){
+function onChangeCheck(e) {
     let anunciosFiltrados = obtenerAnunciosCheckeados(anuncios);
-    anunciosFiltrados = filtrarAnunciosPorEditorial(document.getElementById("selectFiltros").value, anunciosFiltrados);
+    anunciosFiltrados = filtrarAnunciosPorEditorial(
+        document.getElementById("selectFiltros").value,
+        anunciosFiltrados
+    );
     crearTabla(anunciosFiltrados);
     cargarAnuncios(anunciosFiltrados);
-};
+}
 
-function obtenerAnunciosCheckeados(anuncios){
-    return anuncios.map(a => {
-        let anuncioFiltrado = {id : a.id };
+function obtenerAnunciosCheckeados(anuncios) {
+    const checksFiltros = document.querySelectorAll("div input[type=checkbox]");
 
-        checksFiltros.forEach(c =>{
-            if(c.checked){
+    return anuncios.map((a) => {
+        let anuncioFiltrado = { id: a.id };
+
+        checksFiltros.forEach((c) => {
+            if (c.checked) {
                 anuncioFiltrado[c.value] = a[c.value];
             }
-        })
+        });
         return anuncioFiltrado;
     });
 }
 
-function onChangeSelect(e){
-    let anunciosFiltrados = filtrarAnunciosPorEditorial(e.target.value,anuncios);
+function onChangeSelect(e) {
+    let anunciosFiltrados = filtrarAnunciosPorEditorial(
+        e.target.value,
+        anuncios
+    );
     calcularPromedio(anunciosFiltrados);
     anunciosFiltrados = obtenerAnunciosCheckeados(anunciosFiltrados);
     crearTabla(anunciosFiltrados);
     cargarAnuncios(anunciosFiltrados);
-};
+}
 
-function filtrarAnunciosPorEditorial(editorial,anuncios){
+function filtrarAnunciosPorEditorial(editorial, anuncios) {
     let anunciosFiltrados = [];
 
-    if(editorial == "Marvel"){
-        anunciosFiltrados = anuncios.filter( a => a.editorial == "Marvel");
-    }else if (editorial == "Dc Comics"){
-        anunciosFiltrados = anuncios.filter( a => a.editorial != "Marvel");
-    }else{
-        anunciosFiltrados = anuncios.map(a => a);
+    if (editorial == "Marvel") {
+        anunciosFiltrados = anuncios.filter((a) => a.editorial == "Marvel");
+    } else if (editorial == "Dc Comics") {
+        anunciosFiltrados = anuncios.filter((a) => a.editorial != "Marvel");
+    } else {
+        anunciosFiltrados = anuncios.map((a) => a);
     }
 
     return anunciosFiltrados;
-};
+}
 
-function calcularPromedio(anuncios){
-    let fuerza = anuncios.reduce((acc, val) => 
-    acc + parseFloat(val.fuerza),0);
+function calcularPromedio(anuncios) {
+    let fuerza = anuncios.reduce((acc, val) => acc + parseFloat(val.fuerza), 0);
 
     const $txtPromedio = document.getElementById("txtPromedio");
-    $txtPromedio.value = (fuerza / anuncios.length).toFixed(2);
+    $txtPromedio.value = (fuerza / (anuncios.length > 0 ? anuncios.length : 1)).toFixed(2);
 }
